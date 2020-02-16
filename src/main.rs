c34bdcd5b553baa799315a9272c2be0beb91dc62
André <andre.hodapp@t-online.de>
2020-02-16 21:58:38 +0100

    Angriff und Konstruktoren erstellt

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ Elfen -effektiv_gegen-> Orks
 Dies soll die Grundlage für ein größeres
 Kartenspiel sein. Deswegen hat eine Einheit
 folgende Attribute:
-u8: Hp,u8: Damage,u8: Verteidigung,
+i32: Hp,i32: Damage,i32: Verteidigung,
 Enum Rasse: Rasse,Enum Klasse: Klasse,
 Bool: Held, Enum Haeufigkeit: Haeufigkeit
 */
@@ -37,6 +37,7 @@ Es gibt 4 Rassen, wobei Orks und Drachen
 als böse und brutal gelten, wohingegen
 Menschen und Elfen gut und naturnah
 */
+#[derive(Debug)]
 enum Rasse {
     Orks, //-effektiv_gegen-> Menschen
     Menschen, //-effektiv_gegen-> Drachen
@@ -65,6 +66,7 @@ Spielfeld-Aufbau:
         [Fern, Fern]
       [Magier/Heiler]
 */
+#[derive(Debug)]
 enum Klasse {
     Nah, //Krieger kämpfen in vorderster Reihe
     Fern, //Bogenschützen direkt dahinter
@@ -72,6 +74,7 @@ enum Klasse {
     Heiler, //ganz hinten, als Versorgung
 }
 
+#[derive(Debug)]
 enum Haeufigkeit {  // ^
     legendaer,      // |
     episch,         // |
@@ -84,7 +87,7 @@ enum Haeufigkeit {  // ^
 
 /*
 Eineheit hat folgende Attribute:
-u8: Hp,u8: Damage,u8: Verteidigung,
+i32: Hp,i32: Damage,i32: Verteidigung,
 Enum Rasse: Rasse,Enum Klasse: Klasse,
 Bool: Held, Enum Haeufigkeit: Haeufigkeit
 
@@ -97,14 +100,85 @@ auf ihrem Feld besiegt werden.
 Haeufigkeit ist ein Richtwert, wie selten
 diese Einheit als Karte zu finden ist
 */
+#[derive(Debug)]
 struct Einheit {
-    hp: u8, //0-255 HP möglich
-    damage: u8, //0-255 Damage/Heilung möglich
-    verteidigung: u8, //0-255 Verteidigung möglich
+    hp: i32, //logisch soll nur 1-255 HP möglich sein
+    damage: i32, //logisch soll nur 0-255 Damage/Heilung möglich sein
+    verteidigung: i32, //logisch soll nur 0-255 Verteidigung möglich sein
     rasse: Rasse, //Rasse der Einheit
     klasse: Klasse, //Klasse der Einheit
     held: bool, //ist Held: true; kein Held: false
     haeufigkeit: Haeufigkeit, //wie haeufig zu finden
+}
+
+
+/*
+In diesem Block nur Konstruktoren für Einheiten
+*/
+impl Einheit {
+    /*
+        Dies ist ein Konstruktor, welche für die
+        hierbenötigte Aufgabe einen gewöhnlichen
+        Ork-Krieger erstellt
+    */
+    fn gemeiner_Ork(hp: i32, damage: i32,
+    verteidigung: i32) -> Einheit{
+        Einheit {
+            hp, damage, verteidigung, //übergebene Werte
+            rasse: Rasse::Orks,
+            klasse: Klasse::Nah,
+            held: false,
+            haeufigkeit: Haeufigkeit::selten //Ja, richtige Orks sind selten
+        }
+    }
+
+    /*
+        Dies ist ein Konstruktor, welche für die
+        hierbenötigte Aufgabe einen gewöhnlichen
+        Menschen-Banditen erstellt
+    */
+    fn gemeiner_Bandit(hp: i32, damage: i32,
+    verteidigung: i32) -> Einheit{
+        Einheit {
+            hp, damage, verteidigung, //übergebene Werte
+            rasse: Rasse::Menschen,
+            klasse: Klasse::Nah,
+            held: false,
+            haeufigkeit: Haeufigkeit::sehr_haeufig //leider sind Banditen die Pest
+        }
+    }
+
+}
+
+/*
+hier bitte ersmal alle Methoden
+*/
+impl Einheit {
+    /*
+        Einheit soll einen einfachen Angriff durchführen
+        können. (Hier wird noch nicht Felder oder die Klassen betrachtet)
+        Eine Einheit soll
+        Schaden = Einheit:Angriff - Gegner:Verteidigung
+        berechnen und falls Schaden > 0 ist, dem
+        Gegner zufügen, falls Schaden < 0 ist, soll
+        der Angreifer -Schaden viel HP verlieren,
+        falls Schaden = 0, dann soll nichts passieren.
+        In allen Fällen soll false zurückgegeben werden.
+        (Später sollen Felder und Klassen berücksichtigt werden,
+        weswegen dann manche Angriffe grundlegend nicht möglich
+        sein sollten)
+    */
+    fn angriff(&mut self, gegner: &mut Einheit) -> bool {
+        let schaden = self.damage - gegner.verteidigung;
+        let mut error = false;
+        match schaden {
+            0 => println!("kein Schaden angerichtet"),
+            1..=255 => gegner.hp -= schaden,
+            -255..=-1 => self.hp += schaden,
+            _ => error=true,
+        }
+        error
+    }
 }
 
 fn main() {
