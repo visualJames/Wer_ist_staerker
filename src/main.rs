fbc0f13dcc17be6191f46d58caf7bb3ec9e6f0e9
André <andre.hodapp@t-online.de>
2020-02-19 01:25:10 +0100

    Effektivtiät wird jetzt beim Angriff mit berücksichtigt

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ dann soll die angreifende Einheit genau
 so viel Schaden bekommen.
 Es wird abwechselnd angegriffen.
 In der Erweiterung kommt noch ein Bonus
-hinzu beim Angriff oder der Verteidigung,
+hinzu beim Angriff,
 wenn die Einheit effektiv gegen die
 andere Einheit ist.
 Außerdem wenn die Einheit effektiv gegen die
@@ -345,6 +345,51 @@ impl Einheit {
 hier bitte ersmal alle Methoden
 */
 impl Einheit {
+    //bestimmt den zusätzlichen Schaden, wenn die Einheit effektiv ist
+    fn zuesatz_schaden(&self) -> i32{
+        match self.rasse {
+            Rasse::Orks => 5, //Ausgleich dafür, dass sie kaum Rüstung haben
+            Rasse::Menschen => 2,
+            Rasse::Drachen => 0,//Angriff schon so mächtig
+            Rasse::Elfen => 4, //Elfen wissen besonders Orks gut zu schaden
+        }
+    }
+
+    //bestimmt ob die Einheit gegen die Feindeinheit effektiv ist
+    fn ist_effektiv(&mut self, gegner: &Einheit) -> bool {
+        let ist = match self.rasse {
+            Rasse::Orks => {
+                    if let Rasse::Menschen = gegner.rasse{
+                        true
+                    } else {
+                        false
+                    }
+            },
+            Rasse::Menschen => {
+                    if let Rasse::Drachen = gegner.rasse{
+                        true
+                    } else {
+                        false
+                    }
+            },
+            Rasse::Drachen => {
+                    if let Rasse::Elfen = gegner.rasse{
+                        true
+                    } else {
+                        false
+                    }
+            },
+            Rasse::Elfen => {
+                    if let Rasse::Orks = gegner.rasse{
+                        true
+                    } else {
+                        false
+                    }
+            },
+        };
+        ist //ist er effektiv: Ja oder Nein?
+    }
+
     /*
         Einheit soll einen einfachen Angriff durchführen
         können. (Hier wird noch nicht Felder oder die Klassen betrachtet)
@@ -354,6 +399,7 @@ impl Einheit {
         Gegner zufügen, falls Schaden < 0 ist, soll
         der Angreifer -Schaden viel HP verlieren,
         falls Schaden = 0, dann soll nichts passieren.
+        Effektivität der Einheit gegen den Feind wird berücksichtigt.
         In allen Fällen soll false zurückgegeben werden.
         (Später sollen Felder und Klassen berücksichtigt werden,
         weswegen dann manche Angriffe grundlegend nicht möglich
@@ -362,7 +408,20 @@ impl Einheit {
     fn angriff(&mut self, gegner: &mut Einheit) -> bool {
         println!("{} greift mutig {} an",
         self.einheitsbezeichnung, gegner.einheitsbezeichnung);
-        let schaden = self.damage - gegner.verteidigung;
+        //bestimme die Verteidigung des Gegners und den Angriff der eigenen Einheit
+        let mut vert = gegner.verteidigung;
+        let angriff = if self.ist_effektiv(&gegner) {
+            vert = 0; //Gegnerische Rüstung dann wertlos
+            self.damage + self.zuesatz_schaden() //und Zusatsschaden
+        } else {
+            self.damage //kein Zusatsschaden
+        };
+        println!("Der Angriff erfolgt mit {} Angriffschaden von {}
+        und {} Verteidigung von {}", angriff, self.einheitsbezeichnung,
+        vert, gegner.einheitsbezeichnung);
+
+        //füge den Gegner Schaden zu
+        let schaden = angriff - vert;
         let mut error = false;
         match schaden {
             0 => println!("kein Schaden angerichtet"),
